0df05d6b4600543b5fb418b27c004bccc701f02f
Filip Stål <filsta@kth.se>
2018-12-14 19:46:22 +0100

    Lots of stuff
    Basic Pong in AR using ARKit and Unity implemented.
    Simple AI to play against.
    Basic UI and point counters
    Some comments

---
+++ UnityARKit AR Pong/Assets/Scripts/ComputerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputerController : MonoBehaviour {
+
+    public Transform ball;
+
+    [Range(0, 1)]
+    public float skill;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 newPos = transform.position;
+        newPos.x = Mathf.Lerp(transform.position.x, ball.position.x, skill);
+        transform.position = newPos;
+	}
+}
---
+++ UnityARKit AR Pong/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Camera playerCam; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount != 1)
+            return;
+
+        var ray = playerCam.ScreenPointToRay(Input.touches[0].position);
+        var hitInfo = new RaycastHit();
+        if(Physics.Raycast(ray, out hitInfo)) {
+            if (hitInfo.transform.name != "Bounds South")
+                return;
+            var newPos = transform.position;
+            newPos.x = hitInfo.point.x;
+            transform.position = newPos; 
+        }
+
+    }
+}
