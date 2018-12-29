7a1754da52dbbca1b8043aab4821c5efa283ef07
malexn <alexander.nordh@gmail.com>
2018-12-29 16:00:49 +0100

    Add files via upload

---
+++ UnityARKit AR Pong/Assets/Scripts/BallLogic.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BallLogic : MonoBehaviour
+{
+
+    public GameLogic gameLogic;
+
+    //https://docs.unity3d.com/ScriptReference/Vector3.html
+    //Representation of 3D vectors and points.
+    Vector3 velocity;
+
+    [Range(0, 1)]
+    public float speed;  // public makes it vis in unity
+
+    void Start()
+    {
+        ResetBall();
+    }
+
+    public void ResetBall()
+    {
+        transform.position = Vector3.zero; //setting pos to (0,0,0)
+        float z = Random.Range(0, 2) * 2f - 1f;
+        float x = Random.Range(0, 2) * 2f - 1f * Random.Range(0.2f, 1f);
+        float y = Random.Range(0, 2) * 2f - 1f * Random.Range(0.2f, 1f);
+        velocity = new Vector3(x, y, z);
+    }
+    public void Stop()
+    {
+        velocity = Vector3.zero;
+    }
+    // Update is called once per frame
+    // FixedUpdate instead of update to make sure its 
+    // in sync /w physics
+
+    void FixedUpdate()
+    {
+        velocity = velocity.normalized * speed * 0.05f;
+        transform.position += velocity;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    //https://docs.unity3d.com/ScriptReference/Collision.html
+    //https://docs.unity3d.com/ScriptReference/Collider.OnCollisionEnter.html
+    {
+
+        switch (collision.transform.name)
+        {
+            case "Bounds East":
+            case "Bounds West":
+                velocity.x *= -1f;
+                SFXController.PlaySound("wallB");
+                return;
+            case "Bounds Upper":
+            case "Bounds Lower":
+                velocity.y *= -1f;
+                SFXController.PlaySound("wallB");
+                return;
+            case "Bounds North":
+                ResetBall();
+                gameLogic.IncrementScore(collision.transform.name);
+                SFXController.PlaySound("playerScore");
+                return;
+            case "Bounds South":
+                ResetBall();
+                gameLogic.IncrementScore(collision.transform.name);
+                SFXController.PlaySound("compScore");
+                return;
+            case "Player Paddle":
+            case "Computer Paddle":
+                SFXController.PlaySound("paddleB");
+                velocity.z *= -1f;
+                return;
+        }
+    }
+
+}
---
+++ UnityARKit AR Pong/Assets/Scripts/GameLogic.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameLogic : MonoBehaviour {
+
+    public Text txtPlayerScore;
+    public Text txtComputerScore;
+    public int playerScore;
+    public int computerScore;
+    public Text winner;
+    public BallLogic ballLogic;
+    public GameObject Button;
+
+    // public static function so that ball logic can call this 
+    // w/o having to reference the gamelogic
+
+
+    public void IncrementScore(string colliderName) {
+        switch(colliderName) {
+            case "Bounds South":
+                computerScore++;
+                txtComputerScore.text = "Computer: " + computerScore;
+                if (computerScore == 5)
+                    GameOver();
+                    winner.text = "Computer winns!";
+                return;
+            case "Bounds North":
+                playerScore++;
+                txtPlayerScore.text = "Player: " + playerScore;
+                if (playerScore == 5)
+                    GameOver();
+                    winner.text = "Player winns!";
+                return;
+        }
+       
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        Button.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void GameOver()
+    {
+        ballLogic.Stop();
+        Button.SetActive(true);
+    }
+    public void RestartGame()
+    {
+        playerScore = 0;
+        computerScore = 0;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
