2d54a9dba3ca34bb348cdb17d027c892f6920dcd
Viktor Ceder <vceder@kth.se>
2018-12-30 16:09:06 +0100

    add paddle that tracks the AR camera

---
+++ UnityARKit AR Pong/Assets/ARPaddle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARPaddle : MonoBehaviour {
+
+    // Use this for initialization
+
+    public GameObject ARCamera;
+
+    private Vector3 position;
+	void Start () {
+        if(!ARCamera){
+            throw new UnityException("No ARCamera to follow");
+        }
+        position.z = transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        position.x = ARCamera.transform.position.x;
+        position.y = ARCamera.transform.position.y;
+
+        transform.position = position;
+	}
+}
