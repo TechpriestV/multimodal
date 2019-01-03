55dfc8f064f23f6ded2f257e27c32a240f1c4e8d
Viktor Ceder <vceder@kth.se>
2019-01-03 15:20:02 +0100

    changed vital gameobjects to prefabs
    This to ensure consistency across different scenes

---
+++ UnityARKit AR Pong/Assets/Scripts/ControllSettings.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllSettings : MonoBehaviour {
+
+    public enum ControllerTypes {Touch, Gesture};
+    public ControllerTypes crType;
+    public GameObject Touch;
+    public GameObject Gesture;
+
+    //Runs before start, on scene load
+    void Awake () {
+        if(!Touch){
+            throw new UnityException("Need to set gameobject for touch controls!");
+        }
+        if(!Gesture){
+            throw new UnityException("Need to set gameobject for gesture controls!");
+        }
+        if (crType.ToString() == "Touch"){
+            Touch.SetActive(true);
+            Gesture.SetActive(false);
+        }else{
+            Touch.SetActive(false);
+            Gesture.SetActive(true);
+        }
+    }
+}
