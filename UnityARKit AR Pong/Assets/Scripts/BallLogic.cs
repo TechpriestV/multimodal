6450fde671c85c034eb39fb27643b3e865e5a102
Joel Weidenmark <jweidenmark@hotmail.com>
2019-01-07 10:51:15 +0100

    local gitignore

--- UnityARKit AR Pong/Assets/Scripts/BallLogic.cs
+++ UnityARKit AR Pong/Assets/Scripts/BallLogic.cs
@@ -7,6 +7,12 @@ public class BallLogic : MonoBehaviour
 {
 
     public GameLogic gameLogic;
+    public GameObject computerPaddle;
+    public GameObject touchPaddle;
+    public GameObject gesturePaddle;
+
+    public Material Material1;
+    public Material Material2;
 
     //https://docs.unity3d.com/ScriptReference/Vector3.html
     //Representation of 3D vectors and points.
@@ -23,9 +29,14 @@ public class BallLogic : MonoBehaviour
     public void ResetBall()
     {
         transform.position = Vector3.zero; //setting pos to (0,0,0)
-        float z = Random.Range(0, 2) * 2f - 1f;
-        float x = Random.Range(0, 2) * 2f - 1f * Random.Range(0.2f, 1f);
-        float y = Random.Range(0, 2) * 2f - 1f * Random.Range(0.2f, 1f);
+        float z =  1f;
+        //float x = Random.Range(0, 2) * 2f - 1f * Random.Range(0.2f, 1f);
+        //float y = Random.Range(0, 2) * 2f - 1f * Random.Range(0.2f, 1f);
+        float x = Random.Range(-0.3f, 0.3f);
+        float y = Random.Range(-0.3f, 0.3f);
+        //float x = 0f;
+        //float y = 0f;
+
         velocity = new Vector3(x, y, z);
     }
     public void Stop()
@@ -63,23 +74,44 @@ public class BallLogic : MonoBehaviour
                 velocity.y *= -1f;
                 SFXController.PlaySound("wallB");
                 return;
+
+            case "Inner Wall":
+                velocity.z *= -1f;
+                SFXController.PlaySound("wallB");
+                return;
             case "Bounds North":
                 ResetBall();
                 gameLogic.IncrementScore(collision.transform.name);
                 SFXController.PlaySound("playerScore");
                 return;
+
             case "Bounds South":
                 ResetBall();
                 gameLogic.IncrementScore(collision.transform.name);
                 SFXController.PlaySound("compScore");
                 return;
             case "Touch Paddle":
+                SFXController.PlaySound("paddleB");
+                velocity.z *= -1f;
+                StartCoroutine(Flasher(touchPaddle));
+                return;
             case "Gesture Paddle":
+                SFXController.PlaySound("paddleB");
+                velocity.z *= -1f;
+                StartCoroutine(Flasher(gesturePaddle));
+                return;
+
             case "Computer Paddle":
                 SFXController.PlaySound("paddleB");
                 velocity.z *= -1f;
+                StartCoroutine(Flasher(computerPaddle));
                 return;
         }
     }
 
+     IEnumerator Flasher(GameObject paddle) {
+        paddle.GetComponent<MeshRenderer>().material = Material2;
+        yield return new WaitForSeconds(.15f);
+        paddle.GetComponent<MeshRenderer>().material = Material1;
+    }
 }
